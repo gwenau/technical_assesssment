c4f8d2cfdffbe1cc5a7130ac6cb4d9207d9372d1
Gwen Au <quyen_au@hotmail.com>
2016-10-21 09:38:50 +1100

    Two way binding and event actioned on the list

--- dev/clients.component.ts
+++ dev/clients.component.ts
@@ -4,11 +4,24 @@ import {Component} from "angular2/core";
   selector: "client-list",
   template: `
     <ul>
-      <li *ngFor="#clientListItem of clientListItems">{{clientListItem}}</li>
+      <li *ngFor="#clientListItem of clientListItems"
+      (click)="onItemClicked(clientListItem)">
+      {{clientListItem.name}}</li>
     </ul>
+    <input type="text" [(ngModel)]="selectedItem.name">
   `
 })
 
 export class ClientListComponent {
-  public clientListItems = ['Matthew', 'Justin', 'Gramos'];
+  public clientListItems = [
+    {name: 'Matthew'}, 
+    {name: 'Justin'}, 
+    {name: 'Gramos'},
+  ];
+  public selectedItem = {name: ""};
+
+  onItemClicked(clientListItem){
+    this.selectedItem = clientListItem;
+  }
+
 }
