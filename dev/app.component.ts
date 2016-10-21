8d8f291e34ddf40bb9813d5cddfd28a24cb0e7b8
Gwen Au <quyen_au@hotmail.com>
2016-10-21 18:11:58 +1100

    Template separated from the main app.

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,14 +1,17 @@
-import {Component} from 'angular2/core';
+import {Component, View} from 'angular2/core';
 import {ClientListComponent} from './clients.component';
 
 @Component({
-    selector: 'my-app',
-    template: `
-        <h1>Work Ninja</h1>
-        <client-list></client-list>
-    `,
+    selector: 'my-app'
+})
+
+@View({
+    templateUrl: '/templates/landing_page.html',
     directives: [ClientListComponent]
 })
+
 export class AppComponent {
+
+  public client = {name: "", email: "", password: ""}
 
 }
