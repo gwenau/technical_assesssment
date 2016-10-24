202cc80c95dc4fd644b2a66fe8a1d0964981198d
Gwen Au <quyen_au@hotmail.com>
2016-10-24 21:25:47 +1100

    Data is valid now

--- dev/client_add.component.ts
+++ dev/client_add.component.ts
@@ -18,13 +18,16 @@ export class AddClientComponent {
 
   new_client_added = true
 
+  public new_client = {"id": null, "business_name": null, "contact_email": null}
   public id = rootClients.length+1
 
   onAddClient(newClient){
-    newClient["id"] = this.id
-    newClient["business_name"] = newClient.name.value
-    rootClients.push(newClient)
-    newClient = {}
+    
+    this.new_client["id"] = this.id
+    this.new_client["business_name"] = newClient.name.value
+    this.new_client["contact_email"] = newClient.email.value
+    rootClients.push(this.new_client)
+    this.new_client = {}
     this.new_client_added = false
   }
 }
