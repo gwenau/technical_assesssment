5efbbc7383d8258f37935658c073775d12d56b0f
Gwen Au <quyen_au@hotmail.com>
2016-10-24 21:31:29 +1100

    id collection working properly now

--- dev/client_add.component.ts
+++ dev/client_add.component.ts
@@ -18,11 +18,23 @@ export class AddClientComponent {
 
   new_client_added = true
 
-  public new_client = {"id": null, "business_name": null, "contact_email": null}
-  public id = rootClients.length+1
+  public new_client = {
+            id: null, 
+            business_name: null, 
+            abn: null, 
+            contact_email: null, 
+            address: null, 
+            suburb: null, 
+            postcode: null, 
+            country: null, 
+            state: null, 
+            picture: null
+        }
+        
+  public id = null
 
   onAddClient(newClient){
-    
+    this.id = rootClients.length+1
     this.new_client["id"] = this.id
     this.new_client["business_name"] = newClient.name.value
     this.new_client["contact_email"] = newClient.email.value
